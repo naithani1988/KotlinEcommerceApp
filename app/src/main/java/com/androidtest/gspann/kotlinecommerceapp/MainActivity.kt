9f37334ef59eb9b540b196f37b569db126147db4
“Jitendra” <naithani1988@gmail.com>
2019-06-27 11:37:14 +0530

    initial commit

--- app/src/main/java/com/androidtest/gspann/kotlinecommerceapp/MainActivity.kt
+++ app/src/main/java/com/androidtest/gspann/kotlinecommerceapp/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceStateNew: Bundle?) {
+        super.onCreate(savedInstanceStateNew)
         setContentView(R.layout.activity_main)
     }
 }
