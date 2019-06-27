8fd43a218f4155934537d5c408e994d550e4314a
“Jitendra” <naithani1988@gmail.com>
2019-06-27 11:18:33 +0530

    test

---
+++ app/src/main/java/com/androidtest/gspann/kotlinecommerceapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androidtest.gspann.kotlinecommerceapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
